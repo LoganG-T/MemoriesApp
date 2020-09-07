7881c5ae9fab6f256405d48d9fc0b9d73551f741
Logan <logan.gronert@outlook.com>
2020-09-07 14:24:02 +0100

    Started to add the view memories page.

--- app/src/main/java/com/logan/locationrecommender/Activities/ViewMemoryActivity.java
+++ app/src/main/java/com/logan/locationrecommender/Activities/ViewMemoryActivity.java
@@ -1,6 +1,9 @@
 package com.logan.locationrecommender.Activities;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +15,13 @@ public class ViewMemoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_memory);
+
+        //The code below will create a new instance of the memory_view layout and add it to the total memory scroll view
+        LinearLayout linear_layout = findViewById(R.id.linlay_view_all_memories);
+        View new_layout = LayoutInflater.from(this).inflate(
+                R.layout.memory_view_layout, null);
+        linear_layout.addView(new_layout);
+
+
     }
 }
